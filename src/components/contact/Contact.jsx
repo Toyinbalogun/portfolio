f9789874a13e2953fe9e28aca3f9e67ec9813e63
Toyin Balogun <balogunt302@gmail.com>
2021-12-09 01:11:43 -0500

    added new img + modified contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,21 @@
 import "./contact.css"
+import Phone from "../../img/phone.png"
 
 const Contact = () => {
     return (
         <div className="c">
-            contact
+            <div className="c-bg"></div>
+            <div className="c-wrapper">
+                <div className="c-left">
+                    <h1 className="c-title">Let's discuss your project</h1>
+                    <div className="c-info">
+                        <div clasname="c-info-item">
+                            <img src="" alt="" className="" />
+                        </div>
+                    </div>
+                </div>
+                <div className="c-right"></div>
+            </div>
         </div>
     )
 }
