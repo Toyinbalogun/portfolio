4e205766909e8f0ca56ce7e6b9c969cc6c55c470
Toyin Balogun <balogunt302@gmail.com>
2021-12-10 07:25:28 -0500

    made css changes

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const projects = [
       id: 1,
       img: "https://github.com/dustinirving/travel-book/raw/master/resources/travelbook.gif",
       link: "https://www.youtube.com/",
-      para: "AWS image uploader application to upload images from a frontend react app to store images in an Amazon AWS S3 Bucket and user data in a postgreSQL DB. ",
+      para: "AWS image uploader application to upload images from a frontend react app to store images in an AWS S3 Bucket and user data in a postgreSQL DB. ",
       git: "https://github.com/Toyinbalogun/aws-image-uploader",
     },
     {
